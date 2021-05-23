baf20867c5ad6e83716891b4b73b0c9d26cc6161
EHAT32 <enatik14@gmail.com>
2021-05-23 12:43:47 +0300

    Dijkstra with marks

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import numpy as np
 from numpy import random
 
 class DHeap:
-    def __init__(self, alloc_size = 100):
-        self.heap = np.zeros(alloc_size) #Первоначальный размер памяти, выделенной под кучу
-        self.heap_size = 0
-        self.arity = 5
+    def __init__(self, arity = 9, alloc_size = 100):
+        self.__heap = np.zeros(alloc_size) #Первоначальный размер памяти, выделенной под кучу
+        self.__heap_size = 0
+        self.__arity = arity
 
     def siftDown(self, i):
-        while self.arity * i + 1 < self.heap_size:
+        while self.__arity * i + 1 < self.__heap_size:
             min_child = - 1
-            for j in range(self.arity):
-                child = self.arity * i + j + 1
-                if child >= self.heap_size:
+            for j in range(self.__arity):
+                child = self.__arity * i + j + 1
+                if child >= self.__heap_size:
                     break
-                if min_child == -1 or self.heap[min_child] >= self.heap[child]:
+                if min_child == -1 or self.__heap[min_child] >= self.__heap[child]:
                     min_child, child = child, min_child
-            if self.heap[self.arity * i] > self.heap[min_child]:
-                self.heap[self.arity * i], self.heap[min_child] = self.heap[min_child], self.heap[self.arity * i]
+            if self.__heap[self.__arity * i] > self.__heap[min_child]:
+                self.__heap[self.__arity * i], self.__heap[min_child] = self.__heap[min_child], self.__heap[self.__arity * i]
                 i = min_child
             else:
                 break
@@ -28,71 +28,90 @@ class DHeap:
 
 
     def siftUp(self, i):
-        while self.heap[i] < self.heap[int((i - 1) / self.arity)]:
-            self.heap[i], self.heap[int((i - 1) / self.arity)] = self.heap[int((i - 1) / self.arity)], self.heap[i]
-            i = int((i - 1) / self.arity)
+        while self.__heap[i] < self.__heap[int((i - 1) / self.__arity)]:
+            self.__heap[i], self.__heap[int((i - 1) / self.__arity)] = self.__heap[int((i - 1) / self.__arity)], self.__heap[i]
+            i = int((i - 1) / self.__arity)
     
     def extractMin(self):
-        min = self.heap[0]
-        self.heap[0] = self.heap[self.heap_size - 1]
-        self.heap_size = self.heap_size - 1
+        min = self.__heap[0]
+        self.__heap[0] = self.__heap[self.__heap_size - 1]
+        self.__heap_size = self.__heap_size - 1
         self.siftDown(0)
         return min
 
     def __reallocate_heap(self, new_heap_size):
-        if new_heap_size >= np.size(self.heap):
-            heap = np.zeros(np.size(self.heap) * 2)
-            heap[:np.size(heap)] = self.heap
-            self.heap = heap
+        if new_heap_size >= np.size(self.__heap):
+            heap = np.zeros(np.size(self.__heap) * 2)
+            heap[:np.size(heap)] = self.__heap
+            self.__heap = heap
     
     def insert(self, key):
-        self.__reallocate_heap(self.heap_size + 1)
-        self.heap_size = self.heap_size + 1
-        self.heap[self.heap_size - 1] = key
-        self.siftUp(self.heap_size - 1)
+        self.__reallocate_heap(self.__heap_size + 1)
+        self.__heap_size = self.__heap_size + 1
+        self.__heap[self.__heap_size - 1] = key
+        self.siftUp(self.__heap_size - 1)
 
     def Heapify(self, i):
         least = i
-        for j in range(self.arity):
-            child = self.arity * i + j + 1
+        for j in range(self.__arity):
+            child = self.__arity * i + j + 1
             #heap_size - количество элементов в куче
-            if child >= self.heap_size:
+            if child >= self.__heap_size:
                 break
-            if self.heap[child] < self.heap[least]:
+            if self.__heap[child] < self.__heap[least]:
                 least = child
         if least != i:
-            self.heap[i], self.heap[least] = self.heap[least], self.heap[i]
+            self.__heap[i], self.__heap[least] = self.__heap[least], self.__heap[i]
             self.Heapify(least) #self removed
 
     def Build_Heap(self):
-        for i in range(int(self.heap_size / self.arity), -1, -1):
+        for i in range(int(self.__heap_size / self.__arity), -1, -1):
             self.Heapify(i)
 
-    
-    
     @staticmethod
     def merge(a, b):
-        for i in  range(b.heap_size):
-            a.__reallocate_heap(a.heap_size + 1)
-            a.heap_size = a.heap_size + 1
-            a.heap[a.heap_size - 1] = b.heap[i]
+        for i in  range(b.__heap_size):
+            a.__reallocate_heap(a.__heap_size + 1)
+            a.__heap_size = a.__heap_size + 1
+            a.__heap[a.__heap_size - 1] = b.__heap[i]
         a.Build_Heap()
 
+def Dijkstra(Graph, source):
+    vertex_count = np.size(Graph, 0)
+    Q = list(range(vertex_count))
+    dist = [None] * vertex_count
+    prev = [None] * vertex_count                 
+    dist[source] = 0                       
+     
+    while Q:
+        min_idx = np.argmin(Q)
+        u = Q[min_idx]   
+                                            
+        Q.pop(min_idx)
+        
+        for i in range(vertex_count):
+            if Graph[u, i] == None:
+                continue
+            alt = dist[u] + Graph[u, i]
+            if dist[i] == None or alt < dist[i]:              
+                dist[i] = alt
+                prev[i] = u
+
+    return dist, prev
+
 def main():
-    heap1 = DHeap()
-    heap2 = DHeap()
-    
-    heap1.insert(3)
-    heap2.insert(-5)
-    heap2.insert(10)
-    heap1.insert(1)
-    heap1.insert(9)
-    heap2.insert(0.5)
-    heap2.insert(8)
-    heap1.insert(-2)
+    Graph = np.array(
+        [
+            [0   , 7   , 9   , None, None, 14  ], 
+            [7   , 0   , 10  , 15  , None, None],
+            [9   , 10  , 0   , 11  , None, 2   ],
+            [None, 15  , 11  , 0   , 6   , None],
+            [None, None, None, 6   , 0   , 9   ],
+            [14  , None, 2   , None, 9   , 0   ]
+        ]
+    )
 
-
-    DHeap.merge(heap1, heap2)
+    dist, prev = Dijkstra(Graph, 0)
 
     return
 
