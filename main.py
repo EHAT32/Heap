e24975ecdc81792f738bc210712aa48aa4c6204f
EHAT32 <enatik14@gmail.com>
2021-05-26 21:42:14 +0300

    Hoho, working, hehe

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import numpy as np
 from numpy import random
 import time
 
+from numpy.lib.function_base import insert
+
 class DHeap:
-    def __init__(self, arity = 9, alloc_size = 100, dtype = int):
+    def __init__(self, arity = 9, alloc_size = 100, key_selector = None, unique_selector = None):
         self.__arity = arity
-        self.__heap = np.zeros(alloc_size) #Первоначальный размер памяти, выделенной под кучу
+        self.__heap = [None] * alloc_size #Первоначальный размер памяти, выделенной под кучу
+        self.__indices = {}
         self.__heap_size = 0
-        self.__values = np.zeros(alloc_size, dtype = dtype)
-        self.__dtype = dtype
+        if key_selector:
+            self.__select_key = key_selector
+        else:
+            self.__select_key = lambda x: x
+        
+        if unique_selector:
+            self.__select_unique = unique_selector
+        else:
+            self.__select_unique = lambda x: x
 
     def __len__(self):
         return self.__heap_size
@@ -20,11 +30,14 @@ class DHeap:
                 child = self.__arity * i + j + 1
                 if child >= self.__heap_size:
                     break
-                if min_child == -1 or self.__heap[min_child] >= self.__heap[child]:
+                if min_child == -1 or self.__select_key(self.__heap[min_child]) >= self.__select_key(self.__heap[child]):
                     min_child, child = child, min_child
-            if self.__heap[self.__arity * i] > self.__heap[min_child]:
-                self.__heap[self.__arity * i], self.__heap[min_child] = self.__heap[min_child], self.__heap[self.__arity * i]
-                self.__values[self.__arity * i], self.__values[min_child] = self.__values[min_child], self.__values[self.__arity * i]
+            
+            heap_i = self.__select_unique(self.__heap[i])
+            heap_min_child = self.__select_unique(self.__heap[min_child])
+            if heap_i > heap_min_child:
+                self.__indices[heap_i], self.__indices[heap_min_child] = self.__indices[heap_min_child], self.__indices[heap_i]
+                self.__heap[i], self.__heap[min_child] = self.__heap[min_child], self.__heap[i]
                 i = min_child
             else:
                 break
@@ -35,35 +48,42 @@ class DHeap:
 
 
     def siftUp(self, i):
-        while self.__heap[i] < self.__heap[int((i - 1) / self.__arity)]:
+        heap_i = self.__select_unique(self.__heap[i])
+        parent = self.__select_unique(self.__heap[int((i - 1) / self.__arity)])
+        while heap_i < parent:
+            self.__indices[heap_i], self.__indices[parent] = self.__indices[parent], self.__indices[heap_i]
             self.__heap[i], self.__heap[int((i - 1) / self.__arity)] = self.__heap[int((i - 1) / self.__arity)], self.__heap[i]
-            self.__values[i], self.__values[int((i - 1) / self.__arity)] = self.__values[int((i - 1) / self.__arity)], self.__values[i]
             i = int((i - 1) / self.__arity)
     
     def extractMin(self):
-        min_key = self.__heap[0]
-        min_value = self.__values[0]
+        min = self.__heap[0]
+        self.__indices.pop(self.__select_unique(self.__heap[0]))
+        self.__indices[self.__select_unique(self.__heap[self.__heap_size - 1])] = 0
         self.__heap[0] = self.__heap[self.__heap_size - 1]
-        self.__values[0] = self.__values[self.__heap_size - 1]
         self.__heap_size = self.__heap_size - 1
         self.siftDown(0)
-        return min_key, min_value
+        return min
 
     def __reallocate_heap(self, new_heap_size):
         if new_heap_size >= np.size(self.__heap):
-            heap = np.zeros(np.size(self.__heap) * 2)
-            heap[:np.size(heap)] = self.__heap
+            heap = [None] * (len(self.__heap) * 2)
+            heap[:len(self.__heap)] = self.__heap
             self.__heap = heap
-            values = np.zeros(np.size(self.__values) * 2, dtype = self.__dtype)
-            values[:np.size(values)] = self.__values
-            self.__values = values
     
-    def insert(self, key, value):
+    def insert(self, value):
         self.__reallocate_heap(self.__heap_size + 1)
         self.__heap_size = self.__heap_size + 1
-        self.__heap[self.__heap_size - 1] = key
-        self.__values[self.__heap_size - 1] = value
+        self.__heap[self.__heap_size - 1] = value
+        self.__indices[self.__select_unique(value)] = self.__heap_size - 1
         self.siftUp(self.__heap_size - 1)
+
+    def __setitem__(self, value, priority):
+        if value in self.__indices:
+            idx = self.__indices[value]
+            self.__heap[idx] = priority
+            self.Heapify(idx)
+        else:
+            self.insert(priority)
 
     def Heapify(self, i):
         least = i
@@ -72,11 +92,14 @@ class DHeap:
             #heap_size - количество элементов в куче
             if child >= self.__heap_size:
                 break
-            if self.__heap[child] < self.__heap[least]:
+            if self.__select_key(self.__heap[child]) < self.__select_key(self.__heap[least]):
                 least = child
+
         if least != i:
+            heap_i = self.__select_unique(self.__heap[i])
+            heap_least = self.__select_unique(self.__heap[least])
+            self.__indices[heap_i], self.__indices[heap_least] = self.__indices[heap_least], self.__indices[heap_i]
             self.__heap[i], self.__heap[least] = self.__heap[least], self.__heap[i]
-            self.__values[i], self.__values[least] = self.__values[least], self.__values[i]
             self.Heapify(least) #self removed
 
     def Build_Heap(self):
@@ -89,15 +112,16 @@ class DHeap:
             a.__reallocate_heap(a.__heap_size + 1)
             a.__heap_size = a.__heap_size + 1
             a.__heap[a.__heap_size - 1] = b.__heap[i]
-            a.__values[a.__heap_size - 1] = b.__values[i]
+            a.__indices[b.__select_unique(b.__heap[i])] = a.__heap_size - 1
         a.Build_Heap()
 
     @staticmethod
-    def make_dheap(keys, values, arity = 9):
-        dheap = DHeap(arity, alloc_size = 0)
-        dheap.__heap_size = np.size(keys)
-        dheap.__heap = np.copy(keys)
-        dheap.__values = np.copy(values)
+    def make_dheap(values, arity = 9, key_selector = None, unique_selector = None):
+        dheap = DHeap(arity, alloc_size = 0, key_selector=key_selector, unique_selector = unique_selector)
+        dheap.__heap_size = len(values)
+        dheap.__heap = values
+        for i, value in zip(range(len(values)), values):
+            dheap.__indices[dheap.__select_unique(value)] = i
         dheap.__arity = arity
         dheap.Build_Heap()
         return dheap
@@ -109,7 +133,8 @@ def Dijkstra(Graph, source):
     dist = [np.inf] * vertex_count
     prev = [None] * vertex_count                 
     dist[source] = 0                       
-     
+    
+    start = time.perf_counter()
     while Q:
         min_idx = np.argmin(dist)
         u = Q[min_idx]   
@@ -120,9 +145,12 @@ def Dijkstra(Graph, source):
             if Graph[u, i] == None or u == i:
                 continue
             alt = dist[u] + Graph[u, i]
-            if dist[i] == None or alt < dist[i]:              
+            if alt < dist[i]:              
                 dist[i] = alt
                 prev[i] = u
+    stop = time.perf_counter()
+    elapsed_time = (stop - start)
+    print('dijkstra elapsed_time: {} s'.format(elapsed_time))
 
     return dist, prev
 
@@ -130,19 +158,24 @@ def Dijkstra_heap(Graph, source):
     vertex_count = np.size(Graph, 0)
     dist = [np.inf] * vertex_count
     dist[source] = 0                       
-    Q = DHeap.make_dheap(dist, np.arange(0, vertex_count))
-    prev = [None] * vertex_count                 
+    Q = DHeap.make_dheap(list(zip(range(vertex_count), dist)), key_selector=lambda x: x[1], unique_selector=lambda x: x[0])
+    prev = [None] * vertex_count
      
+    start = time.perf_counter()
     while Q:
-        min_dist, min_dist_idx = Q.extractMin()   
+        vertex_idx, min_dist = Q.extractMin()
         for i in range(vertex_count):
-            if Graph[min_dist_idx, i] == None:
+            if Graph[vertex_idx, i] == None:
                 continue
-            alt = min_dist + Graph[min_dist_idx, i]
-            if dist[i] == None or alt < dist[i]:              
+            alt = min_dist + Graph[vertex_idx, i]
+            if alt < dist[i]:              
                 dist[i] = alt
-                prev[i] = min_dist_idx
+                prev[i] = vertex_idx
+                Q[i] = (i, alt)
 
+    stop = time.perf_counter()
+    elapsed_time = (stop - start)
+    print('dijkstra on heap elapsed_time: {} s'.format(elapsed_time))
     return dist, prev
 
 def generate_data(n, m, q, r):
@@ -165,25 +198,22 @@ def generate_data(n, m, q, r):
 
 
 def main():
-
-    #graph = generate_data(10, 1, 10, 20)
-    graph = np.array(
-        [
-            [0   , 7   , 9   , None, None, 14  ], 
-            [7   , 0   , 10  , 15  , None, None],
-            [9   , 10  , 0   , 11  , None, 2   ],
-            [None, 15  , 11  , 0   , 6   , None],
-            [None, None, None, 6   , 0   , 9   ],
-            [14  , None, 2   , None, 9   , 0   ]
-        ]
-    )
-    measurements_count = 100
-    start = time.perf_counter()
-    for i in range(measurements_count):
-        Dijkstra(graph, 0)
-    stop = time.perf_counter()
-    elapsed_time = (stop - start) / measurements_count
-    print(elapsed_time, 's')
+    graph = generate_data(10000, 5000, 1, 2000)
+    #graph = np.array(
+    #    [
+    #        [0   , 7   , 9   , None, None, 14  ], 
+    #        [7   , 0   , 10  , 15  , None, None],
+    #        [9   , 10  , 0   , 11  , None, 2   ],
+    #        [None, 15  , 11  , 0   , 6   , None],
+    #        [None, None, None, 6   , 0   , 9   ],
+    #        [14  , None, 2   , None, 9   , 0   ]
+    #    ]
+    #)
+    measurements_count = 1
+    
+    _, _ = Dijkstra(graph, 0)
+    _, _ = Dijkstra_heap(graph, 0)
+    
     return
 
 if __name__ == '__main__':
