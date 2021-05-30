1718260547d6c75f61f3126701be9aba30f9eb05
EHAT32 <enatik14@gmail.com>
2021-05-30 17:14:09 +0300

    Very working, very good

--- main.py
+++ main.py
@@ -2,31 +2,28 @@ import numpy as np
 from numpy import random
 import time
 import matplotlib.pyplot as plt
-from numpy.core.fromnumeric import sort
-from numpy.testing._private.utils import measure
 from joblib import Parallel, delayed
 import multiprocessing
-from scipy.sparse.construct import rand
 from tqdm import tqdm
 import math
 import random
 import scipy.sparse as sp
 
 class DHeap:
-    def __init__(self, arity = 9, alloc_size = 100, key_selector = None, unique_selector = None):
+    def __init__(self, arity = 9, alloc_size = 100, value = None, key = None):
         self.__arity = arity
-        self.__heap = [None] * alloc_size #Первоначальный размер памяти, выделенной под кучу
+        self.__heap = [None] * alloc_size
         self.__indices = {}
         self.__heap_size = 0
-        if key_selector:
-            self.__select_key = key_selector
+        if value:
+            self.__value = value
         else:
-            self.__select_key = lambda x: x
+            self.__value = lambda x: x
         
-        if unique_selector:
-            self.__select_unique = unique_selector
+        if key:
+            self.__key = key
         else:
-            self.__select_unique = lambda x: x
+            self.__key = lambda x: x
 
     def __len__(self):
         return self.__heap_size
@@ -38,32 +35,27 @@ class DHeap:
                 child = self.__arity * i + j + 1
                 if child >= self.__heap_size:
                     break
-                if min_child == -1 or self.__select_key(self.__heap[min_child]) >= self.__select_key(self.__heap[child]):
+                if min_child == -1 or self.__value(self.__heap[min_child]) >= self.__value(self.__heap[child]):
                     min_child = child
             
-            if self.__select_key(self.__heap[i]) > self.__select_key(self.__heap[min_child]):
-                heap_i = self.__select_unique(self.__heap[i])
-                heap_min_child = self.__select_unique(self.__heap[min_child])
+            if self.__value(self.__heap[i]) > self.__value(self.__heap[min_child]):
+                heap_i = self.__key(self.__heap[i])
+                heap_min_child = self.__key(self.__heap[min_child])
                 self.__indices[heap_i], self.__indices[heap_min_child] = self.__indices[heap_min_child], self.__indices[heap_i]
                 self.__heap[i], self.__heap[min_child] = self.__heap[min_child], self.__heap[i]
                 i = min_child
             else:
                 break
-                
-        #Найти сына, который меньше текущего элемента
-        #Если найдено, меняем местами текущий эл-т и сына
-        #Выполнить siftDown для этого сына
-
 
     def siftUp(self, i):
         while True:
             parent_idx = int((i - 1) / self.__arity)
-            child = self.__select_key(self.__heap[i])
-            parent = self.__select_key(self.__heap[parent_idx])
+            child = self.__value(self.__heap[i])
+            parent = self.__value(self.__heap[parent_idx])
             if child >= parent:
                 break
-            child_u = self.__select_unique(self.__heap[i])
-            parent_u = self.__select_unique(self.__heap[parent_idx])
+            child_u = self.__key(self.__heap[i])
+            parent_u = self.__key(self.__heap[parent_idx])
             self.__indices[child_u], self.__indices[parent_u] = self.__indices[parent_u], self.__indices[child_u]
 
             tmp = self.__heap[i]
@@ -74,8 +66,8 @@ class DHeap:
     
     def extractMin(self):
         min = self.__heap[0]
-        self.__indices.pop(self.__select_unique(self.__heap[0]))
-        self.__indices[self.__select_unique(self.__heap[self.__heap_size - 1])] = 0
+        self.__indices.pop(self.__key(self.__heap[0]))
+        self.__indices[self.__key(self.__heap[self.__heap_size - 1])] = 0
         self.__heap[0] = self.__heap[self.__heap_size - 1]
         self.__heap_size = self.__heap_size - 1
         self.siftDown(0)
@@ -91,13 +83,13 @@ class DHeap:
         self.__reallocate_heap(self.__heap_size + 1)
         self.__heap_size = self.__heap_size + 1
         self.__heap[self.__heap_size - 1] = value
-        self.__indices[self.__select_unique(value)] = self.__heap_size - 1
+        self.__indices[self.__key(value)] = self.__heap_size - 1
         self.siftUp(self.__heap_size - 1)
 
     def __setitem__(self, value, obj):
         idx = self.__indices[value]
-        old_priority = self.__select_key(self.__heap[idx])
-        priority = self.__select_key(obj)
+        old_priority = self.__value(self.__heap[idx])
+        priority = self.__value(obj)
         self.__heap[idx] = obj
         if priority < old_priority:
             self.siftUp(idx)
@@ -108,18 +100,17 @@ class DHeap:
         least = i
         for j in range(self.__arity):
             child = self.__arity * i + j + 1
-            #heap_size - количество элементов в куче
             if child >= self.__heap_size:
                 break
-            if self.__select_key(self.__heap[child]) < self.__select_key(self.__heap[least]):
+            if self.__value(self.__heap[child]) < self.__value(self.__heap[least]):
                 least = child
 
         if least != i:
-            heap_i = self.__select_unique(self.__heap[i])
-            heap_least = self.__select_unique(self.__heap[least])
+            heap_i = self.__key(self.__heap[i])
+            heap_least = self.__key(self.__heap[least])
             self.__indices[heap_i], self.__indices[heap_least] = self.__indices[heap_least], self.__indices[heap_i]
             self.__heap[i], self.__heap[least] = self.__heap[least], self.__heap[i]
-            self.Heapify(least) #self removed
+            self.Heapify(least)
 
     def Build_Heap(self):
         for i in range(int(self.__heap_size / self.__arity), -1, -1):
@@ -135,22 +126,21 @@ class DHeap:
         a.Build_Heap()
 
     @staticmethod
-    def make_dheap(values, arity = 9, key_selector = None, unique_selector = None):
-        dheap = DHeap(arity, alloc_size = 0, key_selector=key_selector, unique_selector = unique_selector)
+    def make_dheap(values, arity = 9, value = None, key = None):
+        dheap = DHeap(arity, alloc_size = 0, value=value, key = key)
         dheap.__heap_size = len(values)
         dheap.__heap = values
-        for i, value in zip(range(len(values)), values):
-            dheap.__indices[dheap.__select_unique(value)] = i
+        for i, value_ in zip(range(len(values)), values):
+            dheap.__indices[dheap.__key(value_)] = i
         dheap.__arity = arity
         dheap.Build_Heap()
         return dheap
 
-def dijkstra(graph : sp.csr_matrix, source):
-    graph_csc = graph.tocsc()
-    vertex_count = graph.get_shape()[0]
+def dijkstra(graph_csr : sp.csr_matrix, source):
+    graph_csc = graph_csr.tocsc()
+    vertex_count = graph_csr.get_shape()[0]
     Q = list(range(vertex_count))
-    dist = [np.inf] * vertex_count
-    #prev = [None] * vertex_count                 
+    dist = [np.inf] * vertex_count                
     dist[source] = 0                       
     
     while Q:
@@ -167,18 +157,17 @@ def dijkstra(graph : sp.csr_matrix, source):
                 if alt < dist[graph.indices[i]]:
                     dist[graph.indices[i]] = alt
 
-        update(graph)
+        update(graph_csr)
         update(graph_csc)
 
-    return dist#, prev
+    return dist
 
 def dijkstra_heap(graph : sp.csr_matrix, source):
     graph_csc = graph.tocsc()
     vertex_count = np.size(graph, 0)
     dist = [np.inf] * vertex_count
     dist[source] = 0                       
-    Q = DHeap.make_dheap(list(zip(range(vertex_count), dist)), key_selector=lambda x: x[1], unique_selector=lambda x: x[0])
-    #prev = [None] * vertex_count
+    Q = DHeap.make_dheap(list(zip(range(vertex_count), dist)), value=lambda x: x[1], key=lambda x: x[0])
      
     while Q:
         u, _ = Q.extractMin()
@@ -195,7 +184,7 @@ def dijkstra_heap(graph : sp.csr_matrix, source):
         update(graph)
         update(graph_csc)
 
-    return dist#, prev
+    return dist
 
 def coor_to_idx(n, i, j):
     return i * (2 * n - i + 1) // 2 + j - i
@@ -216,11 +205,11 @@ def generate_sparse_data(n, m, q, r):
     for i, value in enumerate(random_indices):
         r, c = value
         indices[i] = c
-        for k in range(r - fst_nnz):
+        for _ in range(r - fst_nnz):
             indptr[fst_nnz_idx] = i
             fst_nnz = r
             fst_nnz_idx += 1
-    for k in range(n - fst_nnz):
+    for _ in range(n - fst_nnz):
         indptr[fst_nnz_idx] = m
         fst_nnz_idx += 1
 
@@ -270,42 +259,26 @@ def plots(vertices, time_heap, time_mark):
     
     fig = plt.figure()
 
-    #plt.set_title('Time versus vertices number using heap')
     plt.ylabel('Elapsed time, seconds')
     plt.xlabel('Number of vertices')
     plt.plot(vertices, time_heap)
 
-    #plt.set_title('Time versus vertices number using marks')
     plt.plot(vertices, time_mark)
     plt.legend(["d-heap", "marks"])
 
     plt.show()
 
 def main():
-    # vertices = [0] * 100
-    # time_mark = [0] * 100
-    # time_heap = [0] * 100
 
-    num_cores = multiprocessing.cpu_count()# // 2
-
+    num_cores = 6
     def iter(i):
-        graph = generate_sparse_data(i, 100 * i, 1, 1e6)
+        j = min(1000 * i, int((i - 1) * i / 2))
+        graph = generate_sparse_data(i, j, 1, 1e6)
         elapsed_time_mark = time_counter(lambda : dijkstra(graph, 0), 1)
         elapsed_time_heap = time_counter(lambda : dijkstra_heap(graph, 0), 1)
         return i, elapsed_time_heap, elapsed_time_mark
-    processed_list = Parallel(n_jobs=num_cores)(delayed(iter)(i) for i in tqdm(range(1000, 100000 + 1, 1000)))
+    processed_list = Parallel(n_jobs=num_cores)(delayed(iter)(i) for i in tqdm(range(1000, 10000 + 1, 1000)))
     vertices, time_heap, time_mark = zip(*processed_list)
-
-    #for i in tqdm(range(1000, 100000 + 1, 1000)):
-    #    graph = generate_sparse_data(i, 100 * i, 1, 1e6)
-#
-    #    elapsed_time_mark = time_counter(lambda : dijkstra(graph, 0), 1)
-    #    elapsed_time_heap = time_counter(lambda : dijkstra_heap(graph, 0), 1)
-    #    idx = int((i - 1000) / 1000)
-    #    vertices[idx] = i
-    #    time_mark[idx] = elapsed_time_mark
-    #    time_heap[idx] = elapsed_time_heap
-
     plots(vertices, time_heap, time_mark)
         
     return
